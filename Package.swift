e6d61c1da3cddd93993bd3f53236ec0b9fb00469
Yang Jiao <yang.jiao@photontech.cc>
2026-09-07 15:14:42 +0800

    init: add Package.swift for SPM binary distribution

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "PhotonCheckout",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(name: "PhotonCheckout", targets: ["PhotonCheckoutWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/trustdecision/trustdevice-pro-ios.git", exact: "5.3.1-4"),
+        .package(url: "https://github.com/fingerprintjs/fingerprintjs-ios.git", from: "1.7.0"),
+        .package(url: "https://bitbucket.org/forter-mobile/forter-ios-sdk.git", .upToNextMajor(from: "3.2.2")),
+    ],
+    targets: [
+        .target(
+            name: "PhotonCheckoutWrapper",
+            dependencies: [
+                "PhotonCheckout",
+                .product(name: "TrustDecisionPro", package: "trustdevice-pro-ios"),
+                .product(name: "FingerprintJS", package: "fingerprintjs-ios"),
+                .product(name: "ForterSDK", package: "forter-ios-sdk"),
+            ],
+            path: "Sources/Stub"
+        ),
+        .binaryTarget(
+            name: "PhotonCheckout",
+            url: "https://cdn.photonpay.com/checkout-sdk/ios/v1.0.0/PhotonCheckout.xcframework.zip",
+            checksum: "ded40b4e77964a9b0239662edac55464356139748a0db905ec2b80bfb03c3685"
+        ),
+    ]
+)
