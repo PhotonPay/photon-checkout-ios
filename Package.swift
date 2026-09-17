e6cb71784e91e0796115cb4a456bbefb9798a13c
Yang Jiao <yang.jiao@photontech.cc>
2026-09-17 11:14:00 +0800

    ios sdk v1.0.5

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "PhotonCheckout",
-            url: "https://cdn.photonpay.com/checkout-sdk/ios/v1.0.4/PhotonCheckout.xcframework.zip",
-            checksum: "f3eff41c0ac3bc8cf9c9ab46f7d9d80b40ff3d49cc416ae0702b044a9976199a"
+            url: "https://cdn.photonpay.com/checkout-sdk/ios/v1.0.5/PhotonCheckout.xcframework.zip",
+            checksum: "ed38992f1b9c3f19a2e8535c5076fdb8b7f52283ef5a25c5c5441bd623d320e2"
         ),
     ]
 )
